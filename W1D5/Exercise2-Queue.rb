2791611c92a90138e8602287138baeecf604abfc
Daniela Galvez <danygalis@hotmail.com>
2017-10-06 02:08:07 -0700

    Second file Queue resolved

--- W1D5/Exercise2-Queue.rb
+++ W1D5/Exercise2-Queue.rb
@@ -1,10 +1,22 @@
 class Queue
+  def initialize
+    @queue
+  end
+
   def enqueue(el)
+    @queue << el
+    el
   end
 
   def dequeue
+    @queue.shift
   end
 
   def show
+    @queue.dump
   end
+end
+
+if $PROGRAM_NAME ==__FILE__
+new_queue = Queue.new
 end
