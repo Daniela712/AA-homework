3e54b1cc4b240da7f3012e5e19607230ba3b7bc0
Daniela Galvez <danygalis@hotmail.com>
2017-10-11 22:58:59 -0700

    Dominat_octopus_merge_sort

--- W2D4/big_octopus.rb
+++ W2D4/big_octopus.rb
@@ -1,11 +1,38 @@
 
-def sluggish_octopus(fishes)
+def sluggish_octopus(fishes) #quadratic
   fishes.each_with_index do |f1|
     biggest = true
     fishes.each_with_index do |f2|
-
       biggest = false if f2.length > f1.length
     end
     return f1 if biggest
+  end
+end
+
+class Array
+  def dominat_octopus(&prc) #merge_sort
+    prc ||= Proc.new { |a,b| a <=> b }
+    return self if length < 2
+    mid = self.length/2
+    left = self.take(mid).dominat_octopus(&prc)
+    right = self.drop(mid).dominat_octopus(&prc)
+    Array.merge(left, right, &prc)
+  end
+
+  def merge(left, right, &prc)
+    arr=[]
+    until left.empty? || right.empty?
+      case prc.call(left.first, right.first)
+      when -1
+        arr << left.shift
+      when 0
+        arr << right.shift
+        arr << left.shift
+      when 1
+        arr << right.shift
+      end
+    end
+    arr + left + right
+    arr
   end
 end
